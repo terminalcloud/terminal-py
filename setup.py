fa79f293d4859f7813a1d12a29ea9b61abd0aa55
Administrator <admin@example.com>
2015-03-09 22:45:42 -0300

    licence fixes

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
   name = 'terminal',
-  packages = ['terminal'], # this must be the same as the name above
+  packages = ['terminal'],
   version = '0.1',
   description = 'Terminal.com API wrapper for python',
   author = 'Enrique Conci',
-  author_email = 'qmax.mail@gmail.com',
-  url = 'https://github.com/terminalcloud/terminal-pytho', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
-  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  author_email = 'e@cloudlabs.io',
+  url = 'https://github.com/terminalcloud/terminal-py',
+  download_url = 'https://github.com/terminalcloud/terminal-py/archive/master.zip',
+  keywords = ['terminal', 'terminal.com', 'API'], # arbitrary keywords
   classifiers = [],
 )
